c264298cb0a9a931a225d9f443d4b017b73a2873
Stephen Molloy <stephen.molloy@maxiv.lu.se>
2023-11-02 16:39:18 +0100

    Shorter is better?  Not sure...

--- examples/read_vector.c
+++ examples/read_vector.c
@@ -9,13 +9,9 @@ int main(void) {
   char *filename = "examples/matrix.npy";
   SM_double_array data = SM_new_double_array(256);
 
-  if (get_data_from_npy_file(filename, &data) < 0) {
-    return 1;
-  }
+  if (get_data_from_npy_file(filename, &data) < 0) return 1;
 
-  for (size_t i=0; i<data.length; i++) {
-    printf("%e\n", data.data[i]);
-  }
+  for (size_t i=0; i<data.length; i++) printf("%e\n", data.data[i]);
 
   SM_free(data);
 
